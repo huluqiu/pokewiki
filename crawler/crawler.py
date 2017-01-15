bfcb590a33bf81ed43885f3b3477d8c4b348d940
翻滚的葫芦球 <alezaizai@163.com>
2017-01-15 23:42:46 +0800

    add class->crawler.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,41 +1,91 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import requests
+from lxml import etree
+
 domain = 'https://wiki.52poke.com/'
-listUrl = 'wiki/宝可梦列表（按全国图鉴编号）/简单版'
-# listUrl = '招式列表'
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0.2 Safari/602.3.12'
+path = 'wiki/宝可梦列表（按全国图鉴编号）/简单版'
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0.2 Safari/602.3.12'
+}
+response = requests.get(domain + path, headers=headers)
+root = etree.HTML(response.text)
+table = root.xpath("//div[@id='mw-content-text']/table[1]")[0]
+for child in table[2:]:
+    if len(child) == 1:
+        gen = child.xpath(".//span[@class='mw-headline']")[0]
+        print(gen.text)
+    else:
+        pokemon = child.xpath("td[2]/a[1]")[0]
+        print(pokemon.text)
 
 
-def getHtmlStr(url, domain='', user_agent=''):
-    """get html text from url
+class Crawler(object):
 
-    :url: TODO
-    :domain: TODO
-    :returns: TODO
+    """一个爬虫引擎呀"""
 
-    """
-    from urllib.parse import quote
-    from urllib import request
-    import zlib
+    def __init__(self, pageProcessor):
+        """通过pageProcessor初始化
 
-    url = domain + url
-    url = quote(url, safe='/:')
-    req = request.Request(url)
-    req.add_header('User-Agent', user_agent)
-    with request.urlopen(req) as response:
-        print('Status:', response.status, response.reason)
-        for k, v in response.getheaders():
-            print('%s: %s' % (k, v))
-        content_encoding = response.headers.get('Content-Encoding')
-        if content_encoding == 'gzip':
-            html = zlib.decompress(response.read(), 16 + zlib.MAX_WBITS)
-        html = html.decode('utf-8')
-        print(html)
+        :pageProcessor: TODO
+        :returns: TODO
+
+        """
+        self._pageProcessor = pageProcessor
+
+    def addUrl(self, url):
+        """添加初始url
+
+        :url: TODO
+        :returns: TODO
+
+        """
+        self.url = url
+        return self
+
+    def setScheduler(self, scheduler):
+        """设置scheduler, 用于管理抓取队列
+
+        :scheduler: TODO
+        :returns: TODO
+
+        """
+        self.scheduler = scheduler
+        return self
+
+    def addPipeline(self, pipeline):
+        """用于处理抓取到的数据
+
+        :pipeline: TODO
+        :returns: TODO
+
+        """
+        self.pipeline = pipeline
+        return self
+
+    def run(self):
+        """启动爬虫
+        :returns: TODO
+
+        """
+        self.data = self._pageProcessor()
+        self.scheduler(self.url)
+        self.pipeline(self.data)
 
 
-def getPokewikiHtml(url):
-    return getHtmlStr(url, domain, user_agent)
+def test_pageProcessor():
+    data = {'a': 1, 'b': 2, 'c': 3}
+    return data
 
 
-listHtml = getPokewikiHtml(listUrl)
+def test_scheduler(url):
+    print(url)
+
+
+def test_pipeline(data):
+    for key, value in data.items():
+        print(key, value)
+
+
+Crawler(test_pageProcessor).addUrl(domain).setScheduler(test_scheduler).addPipeline(test_pipeline).run()
