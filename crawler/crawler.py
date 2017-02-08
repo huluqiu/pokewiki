38db286f22970f98b3411e463be82a83dbd59ec7
翻滚的葫芦球 <yongle.lin@outlook.com>
2017-02-08 14:44:38 +0800

    backup(crawler): 52poke

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from gotyou.crawler import Crawler, ConsolePipeline
+from gotyou.crawler import Crawler, Page, FileCacheScheduler, ConsolePipeline, JsonPipeline
 
 domain = 'https://wiki.52poke.com/'
 path = 'wiki/宝可梦列表（按全国图鉴编号）/简单版'
@@ -10,10 +10,41 @@ headers = {
 }
 
 
-def pageProcessor(page):
-    if page.tag is 'detail':
-        namePath = "//*[@id='mw-content-text']/table[2]/tr[1]/td/table/tr/td[1]/span/b/text()"
-        page.addTargetValue('name', page.tree.xpath(namePath))
+def textInXpath(node, xpath):
+    targetNodes = node.xpath(xpath + '/text()')
+    if len(targetNodes) > 0:
+        return targetNodes[0]
+    else:
+        return ''
+
+
+def pageProcessor(page: Page):
+    if page.tag == 'detail':
+        page.addTargetValue('id', textInXpath(page.tree, "//*[@id='mw-content-text']/table[2]/tr[1]/td/table/tr/th/a"))
+        page.addTargetValue('name', textInXpath(page.tree, "//*[@id='mw-content-text']/table[2]/tr[1]/td/table/tr/td[1]/span/b"))
+        page.addTargetValue('name_jap', textInXpath(page.tree, "//*[@id='mw-content-text']/table[2]/tr[1]/td/table/tr/td[1]/b[1]/span"))
+        page.addTargetValue('name_en', textInXpath(page.tree, "//*[@id='mw-content-text']/table[2]/tr[1]/td/table/tr/td[1]/b[2]"))
+        page.addTargetValue('category', textInXpath(page.tree, "//*[@id='mw-content-text']/table[2]/tr[3]/td[2]/table/tr/td/table/tr/td"))
+
+        types = []
+        typesNode = page.tree.xpath("//*[@id='mw-content-text']/table[2]/tr[3]/td[1]/table/tr/td/table/tr/td")
+        if typesNode:
+            for node in typesNode[0]:
+                types.append(node[0].attrib['title'])
+        page.addTargetValue('types', types)
+
+        abilityies = {'normal': [], 'hidden': ''}
+        abilityNode = page.tree.xpath("//*[@id='mw-content-text']/table[2]/tr[4]/td/table/tr/td/table/tr/td[1]")
+        if abilityNode:
+            normals = []
+            for node in abilityNode[0]:
+                if node.tag == 'a':
+                    normals.append(node.text)
+            abilityies['normal'] = normals
+        abilityies['hidden'] = textInXpath(page.tree, "//*[@id='mw-content-text']/table[2]/tr[4]/td/table/tr/td/table/tr/td[2]/a")
+        page.addTargetValue('ability', abilityies)
+        page.addTargetValue('expto100', textInXpath(page.tree, "//*[@id='mw-content-text']/table[2]/tr[5]/td/table/tr/td/table/tr/td"))
+        page.addTargetValue('height', textInXpath(page.tree, "//*[@id='mw-content-text']/table[2]/tr[8]/td[1]/table/tr/td/table/tr/td"))
     else:
         pokemonXpath = "//div[@id='mw-content-text']/table[1]/tr[4]/td[2]/a/attribute::href"
         page.addTargetValue('pokemon', page.tree.xpath(pokemonXpath))
@@ -22,5 +53,7 @@ def pageProcessor(page):
 
 (Crawler(pageProcessor, domain=domain)
         .addRequest(path, headers=headers)
-        .addPipeline(ConsolePipeline)
+        .setScheduler(FileCacheScheduler('./crawler/'))
+        .addPipeline(ConsolePipeline())
+        .addPipeline(JsonPipeline('./crawler/jsons/'))
         .run())
